09a04d076acd670635ab032ab06469a67fa2c1c9
maribhash-sudo <maribhash@gmail.com>
2026-04-21 19:05:00 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const cases = [
     { name: "Budget", price: "500", img: "case1.png" },
-    { name: "Starter", price: "1500", img: "case2.png" },
-    { name: "Basic", price: "2500", img: "case3.png" },
-    { name: "Silver", price: "3500", img: "case4.png" },
+    { name: "Starter", price: "1500", img: "case2.webp" },
+    { name: "Basic", price: "2500", img: "case3.webp" },
+    { name: "Silver", price: "3500", img: "case4.webp" },
     { name: "Gold", price: "4500", img: "case5.png" },
-    { name: "Elite", price: "5500", img: "case6.png" },
-    { name: "Master", price: "6500", img: "case7.png" },
+    { name: "Elite", price: "5500", img: "case6.webp" },
+    { name: "Master", price: "6500", img: "case7.webp" },
     { name: "Pro", price: "7500", img: "case8.png" },
     { name: "Legend", price: "8500", img: "case9.png" },
     { name: "God", price: "10000", img: "case10.png" }
