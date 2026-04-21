e4b137b805d86599175990f1eb621f9d2dc8bfd9
maribhash-sudo <maribhash@gmail.com>
2026-04-21 18:32:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ const cases = [
     { name: "Dragon", price: "500", img: "case4.png.webp" },
     { name: "Glove", price: "800", img: "case5.png.png" },
     { name: "Winter", price: "400", img: "case6.png.webp" },
-    { name: "Summer", price: "400", img: "case7.png.png" },
+    { name: "Summer", price: "400", img: "case7.png.webp" },
     { name: "Gold", price: "1000", img: "case8.png.png" },
     { name: "Mystic", price: "750", img: "case9.png.png" },
     { name: "Pro", price: "2000", img: "case10.png.png" }
 ];
+
+const grid = document.getElementById('cases-grid');
+grid.innerHTML = ""; 
+
+cases.forEach(item => {
+    grid.innerHTML += `
+        <div class="case-card">
+            <img src="img/${item.img}" alt="case" style="width: 80px;">
+            <p>${item.name}</p>
+            <button>${item.price} Coin</button>
+        </div>
+    `;
+});
 
 const grid = document.getElementById('cases-grid');
 cases.forEach(item => {
