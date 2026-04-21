3585fb232ca0bc77c14bbe930a8a5b1041998ba6
maribhash-sudo <maribhash@gmail.com>
2026-04-21 18:15:08 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const tg = window.Telegram.WebApp;
 tg.expand();
 
 const cases = [
-    { name: "Budget Case", price: "50" },
-    { name: "All In", price: "150" },
-    { name: "Pro Case", price: "300" }
+    { name: "Budget Case", price: "50", img: "img/case1.png" },
+    { name: "All In", price: "150", img: "img/case2.png" },
+    { name: "Pro Case", price: "300", img: "img/case3.png" }
 ];
 
 const grid = document.getElementById('cases-grid');
 
-cases.forEach((item, index) => {
+cases.forEach((item) => {
     const card = document.createElement('div');
     card.className = 'case-card';
     card.innerHTML = `
-        <img src="img/case${index + 1}.png" alt="case">
+        <img src="${item.img}" alt="case">
         <p>${item.name}</p>
         <button onclick="openCase('${item.name}')">${item.price} Coin</button>
     `;
