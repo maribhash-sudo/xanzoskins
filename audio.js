0e953b24651075c103c027e4f5f458a2e0077412
maribhash-sudo <maribhash@gmail.com>
2026-04-22 18:15:37 +0500

    audio.js

--- audio.js
+++ audio.js
@@ -1,12 +1,14 @@
 // audio.js
 const sounds = {
-    spin: new Audio('https://www.soundjay.com/buttons/button-50.mp3'),
-    win: new Audio('https://www.soundjay.com/human/applause-01.mp3')
+    spin: new Audio('https://actions.google.com/sounds/v1/ui/click.ogg'), // Ishonchliroq havola
+    win: new Audio('https://actions.google.com/sounds/v1/alarms/digital_watch_alarm_long.ogg')
 };
 
 function playSound(type) {
-    if (sounds[type]) {
-        sounds[type].currentTime = 0; // Ovozni har doim boshidan boshlash
-        sounds[type].play().catch(e => console.log("Ovoz bloklandi, ekranga tegish kerak."));
-    }
+    let s = sounds[type];
+    s.currentTime = 0;
+    s.play().catch(e => {
+        console.error("Ovoz chiqmayapti! Sababi: " + e.message);
+        // Agar ovoz chiqmasa, konsolga xato yozadi
+    });
 }
