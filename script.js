ec5d08b6a8a3eae4e4497625439fc9cc4c148365
maribhash-sudo <maribhash@gmail.com>
2026-04-24 15:51:02 +0500

    script.js

--- script.js
+++ script.js
@@ -258,11 +258,9 @@ function claimDailyBonus() {
             if (dayCount > 30) dayCount = 1;
             let reward = 50 + (dayCount - 1) * 163;
             if (dayCount === 30) reward = 5000;
-            
             updateBalance(Math.round(reward));
             tg.CloudStorage.setItem('lastClaimDate', today);
             tg.CloudStorage.setItem('streakDay', dayCount.toString());
-            
             let msgEl = document.getElementById('bonus-message');
             if(msgEl) msgEl.innerText = dayCount + "-kun: " + Math.round(reward) + " COIN olindi!";
             alert("Tabriklaymiz!");
@@ -287,14 +285,12 @@ function checkNickName() {
     if (!user) { alert("Xatolik!"); return; }
     const fullName = ((user.first_name || "") + " " + (user.last_name || "")).toLowerCase();
     const requiredNick = "@xanzoskins_bot";
-    
     tg.CloudStorage.getItem('lastNickCheck', (err, lastNickCheck) => {
         let now = Date.now();
         if (lastNickCheck && (now - parseInt(lastNickCheck) < 24 * 60 * 60 * 1000)) {
             alert("Siz bugun tekshirdingiz!");
             return;
         }
-        
         if (fullName.includes(requiredNick.toLowerCase())) {
             updateBalance(50);
             tg.CloudStorage.setItem('lastNickCheck', now.toString());
@@ -305,12 +301,9 @@ function checkNickName() {
     });
 }
 
-// BOT ISHGA TUSHGANDA MA'LUMOTLARNI YUKLASH
 document.addEventListener("DOMContentLoaded", () => {
     const tg = window.Telegram.WebApp;
     tg.expand();
-
-    // Balansni yuklash
     tg.CloudStorage.getItem('userBalance', (err, val) => {
         document.getElementById('balance').innerText = val ? val : '10000';
         updateUIBalance();
