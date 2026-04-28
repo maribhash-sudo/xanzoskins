c8ccf2704f89b716ee41b13b5073492978de3403
maribhash-sudo <maribhash@gmail.com>
2026-04-28 19:06:13 +0500

    roulette.js

--- roulette.js
+++ roulette.js
@@ -1,4 +1,4 @@
-console.log("ROULETTE.JS YUKLANDI!");
+console.log("ROULETTE.JS YUKLANDI!"); 
 // Inventarga qo'shish uchun yordamchi funksiya...
 function addToInventory(item) {
     const tg = window.Telegram.WebApp;
